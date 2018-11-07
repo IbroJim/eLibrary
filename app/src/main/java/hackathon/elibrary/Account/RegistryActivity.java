7de09d867dd2751ce97bc8617d686557f5a14b81
IbroJim <salme.i.i115@gmail.com>
2018-11-07 18:50:40 +0300

    create login and registry activity, implemention bottomnavigationviewex

---
+++ app/src/main/java/hackathon/elibrary/Account/RegistryActivity.java
@@ -0,0 +1,16 @@
+package hackathon.elibrary.Account;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import hackathon.elibrary.R;
+
+public class RegistryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registry);
+    }
+}
